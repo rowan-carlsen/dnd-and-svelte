2282e7967990757a3dcb3ec12bb4e618aaf9d0d6
Rowan Carlsen <rowan.carlsen@gmail.com>
2026-03-29 23:50:47 -0400

    add edit to alt nyc map

---
+++ src/lib/EditArea.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import { areaNames } from "./neighborhood-names";
+
+	const { renameArea }: { renameArea: HTMLElement | undefined } = $props();
+	let areaCode: string | undefined = $derived(renameArea?.dataset.area);
+	let dialog: HTMLDialogElement | undefined = $state();
+	$effect(() => {
+		if (areaCode && dialog) {
+			dialog.showModal();
+		}
+	});
+</script>
+
+<dialog bind:this={dialog}>
+	<h2>{areaCode ? areaNames[areaCode].name : "Error: Not Found"}</h2>
+	<div>Name this area: <input type="text" placeholder="Name" /></div>
+	<div class="button-holder"><button type="button">Submit</button></div>
+</dialog>
+
+<style>
+	h2 {
+		text-align: center;
+		margin-top: 0;
+	}
+	.button-holder {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		margin-top: 1rem;
+	}
+	dialog {
+		/* margin: auto; */
+	}
+</style>
--- src/routes/alt-nyc-map/+page.svelte
+++ src/routes/alt-nyc-map/+page.svelte
@@ -16,6 +16,9 @@
 	}
 	:global(body:has(.nyc)) {
 		background-color: #00008baa;
+		/* background-image: url('data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><path stroke="%230070bb" fill="%230070bb" stroke-width=".5" d="M 2 10 A 3 3 0 0 1 8 5 A 2 2 0 0 0 5 10" /></svg>');
+		background-position: top left;
+		background-size: 500px 500px; */
 		padding: 0;
 		height: 100%;
 	}
