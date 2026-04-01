4c5c9e3db5f53bc133e818e825343e584218b1b5
Rowan Carlsen <rowan.carlsen@gmail.com>
2026-03-31 23:09:08 -0400

    base path for prod

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,13 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
 
-export default defineConfig({
-	plugins: [sveltekit()]
+export default defineConfig(({ command }) => {
+	if (command === "serve") {
+		return { plugins: [sveltekit()] };
+	} else {
+		return {
+			base: "/dnd-and-svelte/",
+			plugins: [sveltekit()]
+		};
+	}
 });
